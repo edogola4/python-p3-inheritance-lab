c1a005dc0f4ce85c9dded0678af560949d8e52c4
edogola4 <edogola4@gmail.com>
2025-02-18 16:45:47 +0300

    test passed

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,17 @@
 
 from user import User
 
-class Student(User):
+#class Student(User):
     
-    def learn(self):
-        pass
+ #   def learn(self):
+  #      pass
+
+#from user import User
+
+class Student(User):
+    def __init__(self, first_name, last_name):
+        super(Student, self).__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, info):
+        self.knowledge.append(info)
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,9 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super(Teacher, self).__init__(first_name, last_name)
+        self.knowledge = ["Subject1", "Subject2"]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
